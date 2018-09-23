0f1c9e44e4564f94abf059423d09e2065f51363a
enp256 <ericknp2010@hotmail.com>
2018-09-23 14:56:44 -0300

    ''

--- test_Ic.py
+++ test_Ic.py
@@ -3,10 +3,10 @@ import ic
 
 
 def test_saque():
-    assert ic.saque(200 ,1000) == 84500  
+    assert ic.saque(200 ,1000) == 800  
 
 def test_deposito():
-    assert ic.deposito(50, 1000) == 10450
+    assert ic.deposito(50, 1000) == 1050
 
 def test_Saldo():
-    assert ic.Saldo(1000) == 100022
+    assert ic.Saldo(1000) == 1000
